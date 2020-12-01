793acf3d921fc4963c1b493c20b89bf29a54d673
wzhou2 <wzhou2@stuy.edu>
2020-12-01 00:19:49 -0500

    template

---
+++ template.py
@@ -0,0 +1,21 @@
+input = """
+"""
+
+array = input.split("\n")
+
+def part1():
+    answer = 0
+
+
+    return answer
+
+
+def part2():
+    answer = 0
+
+
+    return answer
+
+
+print(part1())
+# print(part2())
